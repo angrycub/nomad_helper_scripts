bf019455d76852a57986c380f303bac7d97bf4d5
Charlie Voiselle <charlie.voiselle@gmail.com>
2018-08-08 17:51:15 -0400

    Added installer helper

---
+++ install.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+InstallDestination="/usr/local/bin"
+
+function install {
+ if [[ -L ${InstallDestination}/${1} ]]
+  then
+    echo -n "Removing existing symlink and "
+    rm -f ${InstallDestination}/${1}
+  fi
+  echo -n "Creating symlink to ${1} in ${InstallDestination}... " 
+  ln -s `pwd`/${1} ${InstallDestination}/${1}
+  echo "Done."
+}
+
+install cluster_push
+install consul_push
+install nomad_gc
+install nomad_push
+install nomad_reset
+install nomad_start
+install nomad_stop_all  
+install nomad_version
+
